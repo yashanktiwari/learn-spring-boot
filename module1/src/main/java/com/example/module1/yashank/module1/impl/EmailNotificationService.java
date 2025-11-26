d4ea3c3b01ab2ec841c9e9dcc1bcd67f95795f74
Yashank Tiwari <yashanktiwari@gmail.com>
2025-11-26 23:59:09 +0530

    1.3 ConditionalOnProperty example

--- module1/src/main/java/com/example/module1/yashank/module1/impl/EmailNotificationService.java
+++ module1/src/main/java/com/example/module1/yashank/module1/impl/EmailNotificationService.java
@@ -1,11 +1,13 @@
 package com.example.module1.yashank.module1.impl;
 
 import com.example.module1.yashank.module1.NotificationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Primary
+//@Primary
 @Component
+@ConditionalOnProperty(name = "notification.type", havingValue = "email")
 public class EmailNotificationService implements NotificationService {
 
     @Override
--- module1/src/main/java/com/example/module1/yashank/module1/impl/SmsNotificationService.java
+++ module1/src/main/java/com/example/module1/yashank/module1/impl/SmsNotificationService.java
@@ -1,9 +1,11 @@
 package com.example.module1.yashank.module1.impl;
 
 import com.example.module1.yashank.module1.NotificationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "notification.type", havingValue = "sms")
 public class SmsNotificationService implements NotificationService {
 
     @Override
